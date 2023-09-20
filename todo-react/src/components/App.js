e2e96726a2bc41735832f5eeb6760f0758f4a667
NowWeAre-Free <kontodarm@wp.pl>
2023-09-20 19:46:04 +0200

    changed main App component to class component, add some state values

--- todo-react/src/components/App.js
+++ todo-react/src/components/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      TODO APP
-      <AddTask />
-      <TaskList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    tasks: [
+      {
+        id: 0,
+        text: "zagrać w Wiedźmina",
+        date: "22-09-2023",
+        important: false,
+        active: false,
+        finishDate: null,
+      },
+      {},
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        TODO APP
+        <AddTask />
+        <TaskList />
+      </div>
+    );
+  }
 }
 
 export default App;
